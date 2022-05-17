dfaa02601a96c6625b8d09acd50f052908f90dcd
zaak <zaak.kerstetter@gmail.com>
2022-05-17 10:49:30 -0600

    commit tree ff9543581ec50b41a65295fb8641229bfc976852

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+
+console.log(cats)
+
+function destructivelyAppendCat(){
+    cats.push("Ralph")
+    console.log(cats)
+}
+
+function destructivelyPrependCat(){
+    cats.unshift("Bob")
+    console.log(cats)
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop()
+    console.log(cats)
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+    console.log(cats)
+}
+
+function appendCat(){
+    return([...cats, "Broom"])
+}
+
+function prependCat(){
+    return(["Arnold", ...cats])
+}
+
+function removeFirstCat(){
+    return cats.slice(1)
+}
+
+function removeLastCat(){
+    return cats.slice(0, cats.length -1)
+}
